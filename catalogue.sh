acb6554ca33c4fb9a8238913c004c7aed1c559d3
charankumar07 <charankumar0999@gmail.com>
2024-02-13 21:26:39 +0530

    roboshop shell scripts

---
+++ catalogue.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.charan.fun
+
+TIMESTAMP=$(date +F%-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $2....$R FAILURE $N"
+        exit 1
+    else
+        echo -e " $2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " ERROR : $R Please go with root access $N"
+    exit 1
+
+else
+    echo -e " $G you are root user $N"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current Nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling Nodejs:18"
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "EnaInstalling Nodejs:18"
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? "Creating roboshop user"
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading catalogue application"
+
+cd /app &>> $LOGFILE
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Unzipping catalogue"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+#use absolute, because catalogue.service exists there
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
+VALIDATE $? "Copying catalogue service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "Catalogue daemoon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+
+VALIDATE $? "Enabling vatalogue"
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "Starting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+VALIDATE $? "Copying mongodb repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js
+
+VALIDATE $? "Loading catalogue data into mangoDB"
