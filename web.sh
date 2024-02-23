c515b9a04726ebd86b0802fee5f2df0deaeeddf2
charankumar07 <charankumar0999@gmail.com>
2024-02-23 23:56:39 +0530

    roboshop shell scripts

---
+++ web.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.charan.fun
+
+TIMESTAMP=$(date +F%-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $2....$R FAILURE $N"
+        exit 1
+    else
+        echo -e " $2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " ERROR : $R Please go with root access $N"
+    exit 1
+
+else
+    echo -e " $G you are root user $N"
+fi
+
+dnf install nginx -y
+
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx
+
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx
+
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/*
+
+VALIDATE $? "Remove default website"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+
+VALIDATE $? "Downloaded web application"
+
+cd /usr/share/nginx/html
+
+VALIDATE $? "Moving nginx html directory"
+
+unzip -o /tmp/web.zip
+
+VALIDATE $? "unziping web"
+
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+VALIDATE $? "Copied roboshop reverse proxy config"
+
+systemctl restart nginx
+
+VALIDATE $? "Restarted nginx"
+
+
+
