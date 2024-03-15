13df1f452334925529dcf78b49e02c918e5d939a
charankumar07 <charankumar0999@gmail.com>
2024-03-15 23:12:39 +0530

    roboshop shell scripts

--- roboshop.sh
+++ roboshop.sh
@@ -12,6 +12,7 @@ do
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
+    fi
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0280acde162fb3c29
 done
