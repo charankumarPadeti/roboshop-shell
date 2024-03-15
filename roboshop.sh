ebd3acdbc8e0f7017d32b08d4040c2c9d95c9318
charankumar07 <charankumar0999@gmail.com>
2024-03-15 23:06:26 +0530

    roboshop shell scripts

---
+++ roboshop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-0280acde162fb3c29
+INSTANCES=("mangodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mangodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0280acde162fb3c29
+done
