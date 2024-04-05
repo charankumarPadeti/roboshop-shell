6037c63e8993f822cefecc18f6f598318342c813
charankumar07 <charankumar0999@gmail.com>
2024-04-06 00:48:54 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-0280acde162fb3c29 #replace with your SG ID
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-0280acde162fb3c29
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-DOMAIN_NAME="charan.fun"
+ZONE_ID=Z06442052M5X8F6RMTTUG
+DOAMIN_NAME=charan.fun
 
 for i in "${INSTANCES[@]}"
 do
+    echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -15,19 +16,18 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0280acde162fb3c29 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0280acde162fb3c29 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
     {
         "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
-        "Action"              : "UPSERT"
+        "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            "Name"              : "'$i'.'$DOAMIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
@@ -35,6 +35,6 @@ do
             }]
         }
         }]
-    }
+    }   
         '
 done
